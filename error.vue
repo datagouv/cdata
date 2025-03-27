dc996584f875ec4dfdcf47bdba37bdc110bb5d6b
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-03-27 09:34:13 +0100

    fix: config error on error page

--- error.vue
+++ error.vue
@@ -27,7 +27,40 @@
   </NuxtLayout>
 </template>
 
-<script setup>
-const { t } = useI18n()
+<script setup lang="ts">
+import { datagouv } from '@datagouv/components-next'
+import type { UseFetchFunction } from '@datagouv/components-next'
+import { NuxtLinkLocale, TextClamp } from '#components'
+
 const error = useError()
+
+const app = useNuxtApp()
+
+const i18nHead = useLocaleHead()
+const runtimeConfig = useRuntimeConfig()
+
+app.vueApp.use(datagouv, {
+  name: runtimeConfig.public.title,
+  baseUrl: runtimeConfig.public.i18n.baseUrl, // Maybe do not use i18n config here?
+  apiBase: runtimeConfig.public.apiBase,
+  devApiKey: runtimeConfig.public.devApiKey,
+  staticUrl: runtimeConfig.public.staticUrl,
+  tabularApiUrl: 'https://tabular-api.data.gouv.fr',
+  tabularApiAllowRemote: true,
+  datasetQualityGuideUrl: runtimeConfig.public.datasetQualityGuideUrl,
+  customUseFetch: useAPI as UseFetchFunction, // Why this `as` is required?
+  textClamp: TextClamp,
+  appLink: NuxtLinkLocale,
+})
+
+useHeadSafe({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs!.lang,
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])],
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - data.gouv.fr` : 'data.gouv.fr'
+  },
+})
 </script>
