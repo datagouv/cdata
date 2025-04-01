5e89c53356b1de8c5aaa3cd7d2e40b8e4f8dd0cf
Thibaud Dauce <ThibaudDauce@users.noreply.github.com>
2025-04-01 10:35:00 +0200

    Add email to profile page header (#379)

--- components/AdminEmail.vue
+++ components/AdminEmail.vue
@@ -1,8 +1,23 @@
 <template>
-    <p class="px-1 rounded-2 monospace text-sm text-gray-medium bg-gray-lower m-0">{{ email }}</p>
+  <p
+    v-if="'email' in user && user.email "
+    class="fr-m-0 fr-text--xs text-mention-grey f-italic inline-flex items-center"
+  >
+    <RiMailLine class="size-3" />
+    <TextClamp
+      class="fr-px-1v"
+      :text="user.email as string"
+      :auto-resize="true"
+      :max-lines="1"
+    />
+  </p>
 </template>
+
 <script setup lang="ts">
+import { RiMailLine } from '@remixicon/vue'
+import type { User } from '@datagouv/components-next'
+
 defineProps<{
-    email: string;
-}>();
+  user: User
+}>()
 </script>
