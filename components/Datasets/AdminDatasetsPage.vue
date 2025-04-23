7c60859a5d470a806626e7cb1a7dc921f7b721c3
maudetes <maudet.estelle@gmail.com>
2025-04-23 11:17:58 +0200

    Add dataset status filter (#393)
    Based on https://github.com/opendatateam/udata/pull/3298

--- components/Datasets/AdminDatasetsPage.vue
+++ components/Datasets/AdminDatasetsPage.vue
@@ -31,6 +31,14 @@
         </h2>
       </div>
       <div class="flex-none flex flex-wrap items-center md:gap-x-6 gap-2">
+        <SearchableSelect
+          v-model="datasetsStatus"
+          :placeholder="$t('Filter by status')"
+          :options="statusOption"
+          :display-value="(option) => option.label"
+          :multiple="false"
+          class="mb-0"
+        />
         <AdminInput
           v-model="q"
           type="search"
@@ -68,19 +76,28 @@
 
     <div
       v-if="status != 'pending' && pageData && !pageData.total"
-      class="flex flex-col items-center"
+      class="flex flex-col items-center fr-my-3v"
     >
       <nuxt-img
         src="/illustrations/dataset.svg"
         class="h-20"
       />
-      <template v-if="q">
-        <p class="fr-text--bold fr-my-3v">
+      <template v-if="q || datasetsStatus">
+        <p
+          v-if="q"
+          class="fr-text--bold fr-my-3v"
+        >
           {{ t(`No results for "{q}"`, { q }) }}
+        </p>
+        <p
+          v-else
+          class="fr-text--bold fr-my-3v"
+        >
+          {{ t('No results') }}
         </p>
         <BrandedButton
           color="primary"
-          @click="q = qDebounced = ''"
+          @click="resetFilters"
         >
           {{ $t('Reset filters') }}
         </BrandedButton>
@@ -121,14 +138,36 @@ const direction = ref<SortDirection>('desc')
 const sortDirection = computed(() => `${direction.value === 'asc' ? '' : '-'}${sortedBy.value}`)
 const q = ref('')
 const qDebounced = refDebounced(q, 500) // TODO add 500 in config
+const datasetsStatus = ref<string | null>(null)
+
+const statusOption = [{
+  label: t('Public'),
+  id: 'public',
+}, {
+  label: t('Archived'),
+  id: 'archived',
+}, {
+  label: t('Draft'),
+  id: 'private',
+}, {
+  label: t('Deleted'),
+  id: 'deleted',
+}]
+
 
 function sort(column: DatasetSortedBy, newDirection: SortDirection) {
   sortedBy.value = column
   direction.value = newDirection
 }
 
+function resetFilters() {
+  q.value = ''
+  qDebounced.value = ''
+  datasetsStatus.value = null
+}
+
 const params = computed(() => {
-  return {
+  const query = {
     organization: props.organization?.id,
     owner: props.user?.id,
 
@@ -137,6 +176,25 @@ const params = computed(() => {
     page_size: pageSize.value,
     page: page.value,
   }
+
+  if (datasetsStatus.value) {
+    if (datasetsStatus.value.id === 'public') {
+      query['archived'] = 'false'
+      query['deleted'] = 'false'
+      query['private'] = 'false'
+    }
+    if (datasetsStatus.value.id === 'deleted') {
+      query['deleted'] = 'true'
+    }
+    if (datasetsStatus.value.id === 'archived') {
+      query['archived'] = 'true'
+    }
+    if (datasetsStatus.value.id === 'private') {
+      query['private'] = 'true'
+    }
+  }
+
+  return query
 })
 
 const { data: pageData, status, refresh } = await useAPI<PaginatedArray<DatasetV2>>('/api/2/datasets/', { lazy: true, query: params })
