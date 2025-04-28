25b372de7de61c812f172d32cfade2e9d09321ae
Thibaud Dauce <git@hadibut.fr>
2025-04-28 17:14:40 +0200

    Add discussions

--- pages/dataservices/[did]/discussions.vue
+++ pages/dataservices/[did]/discussions.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>Discussions</div>
+  <DiscussionsList
+    :subject="dataservice"
+    type="Dataservice"
+  />
 </template>
 
 <script setup lang="ts">
+import type { Dataservice } from '@datagouv/components-next'
+import DiscussionsList from '~/components/Discussions/DiscussionsList.vue'
+
+defineProps<{ dataservice: Dataservice }>()
 </script>
