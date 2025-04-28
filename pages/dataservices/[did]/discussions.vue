eedcd8a96a629ba450a0d01f9c50b19386255778
Thibaud Dauce <git@hadibut.fr>
2025-04-28 13:54:38 +0200

    Show datasets

---
+++ pages/dataservices/[did]/discussions.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>Discussions</div>
+</template>
+
+<script setup lang="ts">
+</script>
--- pages/dataservices/[did]/index.vue
+++ pages/dataservices/[did]/index.vue
@@ -1,6 +1,47 @@
 <template>
-  <div>Hello</div>
+  <div
+    v-if="status === 'success'"
+    class="flex flex-wrap justify-between items-center mb-5"
+  >
+    <h2 class="text-sm font-bold uppercase m-0 text-gray-title">
+      {{ $t('{n} datasets', pageData.total) }}
+    </h2>
+
+    <div class="grid grid-cols-2 gap-5">
+      <DatasetCardLg
+        v-for="dataset in pageData.data"
+        :key="dataset.id"
+        :dataset
+        :show-description="false"
+      />
+    </div>
+    <div class="w-full mt-6 flex justify-center">
+      <Pagination
+        :page="page"
+        :page-size="pageSize"
+        :total-results="pageData.total"
+        @change="(changedPage: number) => page = changedPage"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { Pagination, type Dataservice, type DatasetV2 } from '@datagouv/components-next'
+import type { PaginatedArray } from '~/types/types'
+
+const props = defineProps<{ dataservice: Dataservice }>()
+
+const pageSize = 6
+const page = ref(1)
+
+const query = computed(() => {
+  return {
+    dataservice: props.dataservice.id,
+    page: page.value,
+    page_size: pageSize,
+  }
+})
+
+const { data: pageData, status } = await useAPI<PaginatedArray<DatasetV2>>('/api/2/datasets', { query })
 </script>
