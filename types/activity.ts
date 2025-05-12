97500ae6f0a61d3c300d1ecbb721d2cca166b28c
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-05-12 10:30:22 +0200

    feat: add missing activity keys

--- types/activity.ts
+++ types/activity.ts
@@ -1,6 +1,8 @@
 import type { Organization, User } from '@datagouv/components-next'
 
 export type ActivityKey = 'dataset:created' | 'dataset:updated' | 'dataset:deleted' | 'dataset:discussed' | 'dataset:followed'
+  | 'dataset:resource:added' | 'dataset:resource:updated' | 'dataset:resource:deleted'
+  | 'dataservice:created' | 'dataservice:updated' | 'dataservice:deleted' | 'dataservice:discussed' | 'dataservice:followed'
   | 'organization:created' | 'organization:updated' | 'organization:followed'
   | 'reuse:created' | 'reuse:updated' | 'reuse:deleted' | 'reuse:discussed' | 'reuse:followed'
   | 'user:followed'
