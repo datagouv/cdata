842dc25f706b2b5562430bcf00d13672fd4c4fe0
Thibaud Dauce <ThibaudDauce@users.noreply.github.com>
2025-06-17 14:59:15 +0200

    Improve user profile page (#491)
    - [x] Add subtitles
    - [x] Add favorites (require
    https://github.com/opendatateam/udata/pull/3337)
    - [x] Improve types
    - [x] Add noindex on page
    - [x] Add page title

--- datagouv-components/src/types/users.ts
+++ datagouv-components/src/types/users.ts
@@ -1,10 +1,22 @@
+import type { Organization } from "./organizations"
+
 export type User = {
-  id: string;
-  slug?: string;
-  first_name: string;
-  last_name: string;
-  avatar?: string | null;
-  page: string;
-  avatar_thumbnail?: string | null;
-  roles?: Array<string>;
+  id: string
+  slug?: string
+  first_name: string
+  last_name: string
+  about: string
+  avatar?: string | null
+  page: string
+  avatar_thumbnail?: string | null
+  roles?: Array<string>
+  organizations: Array<Organization>
+  metrics: {
+    datasets: number
+    dataservices: number
+    reuses: number
+
+    followers: number
+    following: number
+  }
 }
