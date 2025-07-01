407deba4cbbe21cbf0c0b8b21efdd409f01f5c57
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-07-01 12:11:13 +0200

    refac: remove console.log

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -1,5 +1,4 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
-  console.log(to.path)
   if (to.path.startsWith('/fr/') || to.path.startsWith('/en/') || to.path.startsWith('/es/')) {
     return navigateTo(to.path.slice(3), {
       redirectCode: 308,
