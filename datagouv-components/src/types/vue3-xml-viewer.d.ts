d4e04e476583ba97095235a76aecc4fff0236892
Adrien Carpentier <me@adriencarpentier.com>
2025-07-09 11:51:21 +0200

    clean: remove useless props to xmlviewer

--- datagouv-components/src/types/vue3-xml-viewer.d.ts
+++ datagouv-components/src/types/vue3-xml-viewer.d.ts
@@ -4,18 +4,6 @@ declare module 'vue3-xml-viewer' {
   export interface XmlViewerProps {
     xml: string
     theme?: 'light' | 'dark'
-    indentWidth?: number
-    showLineNumbers?: boolean
-    showAttributes?: boolean
-    showComments?: boolean
-    showCdata?: boolean
-    showDoctype?: boolean
-    showProcessingInstructions?: boolean
-    showTextNodes?: boolean
-    showWhitespace?: boolean
-    expandAll?: boolean
-    maxDepth?: number
-    expandDepth?: number
   }
 
   export const XmlViewer: Component<XmlViewerProps>
