9eda4a8394b555f457e540da424d1fe3b0676990
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-07-25 15:56:08 +0200

    refac: same logic for org search

--- types/form.ts
+++ types/form.ts
@@ -44,6 +44,13 @@ export type DataserviceSearchParams = {
   page_size?: string
 }
 
+export type OrganizationSearchParams = {
+  q?: string
+  sort?: string
+  page?: string
+  page_size?: string
+}
+
 export type ReuseSearchParams = {
   q?: string
   sort?: string
