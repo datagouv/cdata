5af98521da100cf092078ea0c9292077a77c6202
Nicolas KEMPF <nicolas@conciergerie.dev>
2025-07-29 09:57:11 +0200

    Use same logic for all 3 searches (#613)

--- pages/reuses/index.vue
+++ pages/reuses/index.vue
@@ -27,9 +27,11 @@
 
 <script setup lang="ts">
 import type { Reuse, ReuseTopic, Site } from '@datagouv/components-next'
+import { useUrlSearchParams } from '@vueuse/core'
 import type { LocationQueryValue } from 'vue-router'
 import BreadcrumbItem from '~/components/Breadcrumbs/BreadcrumbItem.vue'
 import ListPage from '~/components/Reuses/ListPage.vue'
+import type { ReuseSearchParams } from '~/types/form'
 import type { PaginatedArray } from '~/types/types'
 
 const { t } = useI18n()
@@ -39,34 +41,35 @@ useSeoMeta({
 })
 
 const route = useRoute()
+const params = useUrlSearchParams<ReuseSearchParams>('history', {
+  initialValue: route.query,
+  removeNullishValues: true,
+  removeFalsyValues: true,
+})
+
 const q = ref('')
 watchEffect(() => {
   if (Array.isArray(route.query.q)) return
   if (!route.query.q) return
   q.value = route.query.q
 })
+
 const sort = ref((route.query.sort as string | null) || undefined)
 const tag = ref((route.query.tag as string | null) || undefined)
 const topic = ref((route.query.topic as string | null) || undefined)
 const page = ref(parseInt(route.query.page as LocationQueryValue ?? '1', 10))
 const pageSize = 21
 
+const nonFalsyParams = computed(() => {
+  const filteredParams = Object.entries(toValue(params)).filter(([_k, v]) => v)
+  return { ...Object.fromEntries(filteredParams), page_size: pageSize }
+})
+
 function change(newQs: string, newTopic: string | undefined, newSort: string | undefined, newPage: number) {
   q.value = newQs
   sort.value = newSort
   page.value = newPage
   topic.value = newTopic
-  return navigateTo({
-    ...route,
-    query: {
-      ...route.query,
-      q: q.value,
-      page: page.value,
-      sort: sort.value,
-      tag: tag.value,
-      topic: topic.value,
-    },
-  })
 }
 
 const { data: site } = await useAPI<Site>('/api/1/site')
@@ -77,13 +80,14 @@ const { data: reuses, status } = await useAPI<PaginatedArray<Reuse>>(`/api/2/reu
   headers: {
     'X-Fields': reusesXFields,
   },
-  params: {
-    q,
-    page,
-    page_size: pageSize,
-    sort,
-    tag,
-    topic,
-  },
+  params: nonFalsyParams,
+})
+
+watchEffect(() => {
+  if (page.value > 1 || params.page) params.page = page.value.toString()
+  params.q = q.value
+  params.sort = sort.value
+  params.tag = tag.value
+  params.topic = topic.value
 })
 </script>
