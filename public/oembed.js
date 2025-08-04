b3d9ba3be90e92f7b3d1990119d984660fc4054c
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2025-08-04 09:00:58 +0200

    remove: lang for oembeds

--- public/oembed.js
+++ public/oembed.js
@@ -36,8 +36,6 @@ function getSize(attr) {
 const BASE_URL = getBaseUrl();
 // OEmbed endpoint URL
 const OEMBED_URL = `${BASE_URL}/nuxt-api/oembed`;
-// Loads cards in the same language than the current site
-const LANG = document.documentElement.lang;
 // Supported attributes
 const ATTRS = ['dataservice', 'dataset', 'organization', 'reuse',];
 
@@ -85,7 +83,7 @@ function toTitle(txt) {
             div.innerHTML = `
             <svg width="24" height="24" style="color: #3558a2; animation: spin 1s linear infinite;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>`;
             const id = div.dataset[`udata${toTitle(attr)}`];
-            fetchOEmbed(`${BASE_URL}/${LANG}/${attr}s/${id}/`, attr)
+            fetchOEmbed(`${BASE_URL}/${attr}s/${id}/`, attr)
                 .then(oembed => {
                     div.innerHTML = oembed.html;
                 });
