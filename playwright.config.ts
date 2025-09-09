ca2a68f757a7525df66390120b7e368a41a7bd42
Thibaud Dauce <git@hadibut.fr>
2025-09-09 13:46:08 +0200

    Try to increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,6 +21,12 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
+  timeout: 40_000,
+  expect: {
+    timeout: 10_000,
+  },
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -39,10 +45,10 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
