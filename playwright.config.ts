8df0925a0d83073db49adb4329fd93868e2b7b92
Thibaud Ollagnier <ThibaudDauce@users.noreply.github.com>
2025-09-10 14:03:27 +0200

    Redirects if search filters are present (#675)
    - Fix https://github.com/datagouv/data.gouv.fr/issues/1859
    - [x] Logs udata and cdata logs in CI
    - [x] Fix incorrect NUXT_PUBLIC_API_BASE
    - [x] Reuse existing server in CI
    
    Do not work for organization, schemas and licenses because of the `lazy:
    true`. Will fix it in another PR because it also breaks on
    https://dev.data.gouv.fr/datasets/search?organization=5abca8d588ee386ee6ece479
    (the org id is not in the fist page)

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,11 +47,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: process.env.CI
-    ? undefined
-    : {
-        command: 'npm run dev',
-        url: 'http://localhost:3000',
-        reuseExistingServer: !process.env.CI,
-      },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: true,
+  },
 })
