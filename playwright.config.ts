0575caf6f20f1355cfb38349d55c66d877d8769c
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-09-23 15:36:35 +0200

    test: add action and navigation timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    actionTimeout: 10_000,
+    navigationTimeout: 30_000,
   },
 
   /* Configure projects for major browsers */
