1bfdf1430d57635060b2dc03b24f05fe03a5ba2e
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-10-01 15:48:44 +0200

    feat: add user/:id activities

---
+++ pages/admin/users/[uid]/profile/activities.vue
@@ -0,0 +1,10 @@
+<template>
+  <ActivityList :user="user.id" />
+</template>
+
+<script setup lang="ts">
+import type { User } from '@datagouv/components-next'
+import ActivityList from '~/components/ActivityList.vue'
+
+defineProps<{ user: User }>()
+</script>
