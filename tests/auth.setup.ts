51c06100616f170133a55f19bd4856765d520527
Thibaud Dauce <git@hadibut.fr>
2025-10-06 11:36:26 +0200

    Replace dev.local to localhost

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -5,11 +5,11 @@ const authFile = path.join(import.meta.dirname, '../playwright/.auth/user.json')
 
 setup('authenticate', async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
-  await page.goto('http://dev.local:7000/login/')
+  await page.goto('http://localhost:7000/login/')
   await page.getByLabel('Adresse email').fill('admin@example.com')
   await page.getByLabel('Mot de passe').fill('@1337Password42')
   await page.getByRole('button', { name: 'Se connecter' }).click()
-  await page.waitForURL('http://dev.local:7000/')
+  await page.waitForURL('http://localhost:7000/')
 
   // End of authentication steps.
 
