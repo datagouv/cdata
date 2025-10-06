1434342c4382715946700f0eb568f6e2fe61eee4
Thibaud Dauce <git@hadibut.fr>
2025-10-06 13:59:46 +0200

    Try to work on local and CI

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -3,14 +3,19 @@ import path from 'path'
 
 const authFile = path.join(import.meta.dirname, '../playwright/.auth/user.json')
 
-setup('authenticate', async ({ page }) => {
-  // Perform authentication steps. Replace these actions with your own.
-  await page.goto('http://localhost:7000/login/')
+setup('authenticate', async ({ page, baseURL }) => {
+  // CI use localhost but local use dev.local so check this with the baseURL which should
+  // be correctly configured.
+  const loginURL = baseURL?.replace(':3000', ':7000') || 'http://localhost:7000'
+
+  await page.goto(`${loginURL}/login/`)
   await page.getByLabel('Adresse email').fill('admin@example.com')
   await page.getByLabel('Mot de passe').fill('@1337Password42')
-  await page.screenshot({ path: 'screenshot.png', fullPage: true })
-  await page.getByRole('button', { name: 'Sign in' }).click() // I don't know why the button is in english and the input labels in french.
-  await page.waitForURL('http://localhost:7000/')
+
+  // I don't know why the button is in english and the input labels in french.
+  // In local, it's "Se connecter", in CI "Sign in"
+  await page.getByRole('button', { name: /(Sign in|Se connecter)/ }).click()
+  await page.waitForURL(`${loginURL}/`)
 
   // End of authentication steps.
 
