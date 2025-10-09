5168ccad29eb4e32a054be417345ddfab4a3988e
Thibaud Ollagnier <ThibaudDauce@users.noreply.github.com>
2025-10-09 14:27:03 +0200

    test: datasets creation (#725)

--- components/Datasets/DescribeDataset.vue
+++ components/Datasets/DescribeDataset.vue
@@ -405,6 +405,7 @@
               :required="true"
               :error-text="getFirstError('frequency')"
               :warning-text="getFirstWarning('frequency')"
+              data-testid="select-frequency"
             />
             <SimpleBanner
               v-if="getFirstWarning('frequency')"
@@ -609,7 +610,7 @@ const chooseFrequencyAccordionId = useId()
 const addTemporalCoverageAccordionId = useId()
 const addSpatialInformationAccordionId = useId()
 
-const { data: frequencies } = await useAPI<Array<Frequency>>('/api/1/datasets/frequencies', { lazy: true })
+const { data: frequencies } = await useAPI<Array<Frequency>>('/api/1/datasets/frequencies/', { lazy: true })
 
 const { data: allLicenses } = await useAPI<Array<License>>('/api/1/datasets/licenses', { lazy: true })
 
