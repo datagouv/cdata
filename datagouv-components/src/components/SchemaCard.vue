b94876ec5a77372bd2fbedddbe0c2dda4cb85af8
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-11-07 10:02:00 +0100

    fix(schema): redesign SchemaCard

--- datagouv-components/src/components/SchemaCard.vue
+++ datagouv-components/src/components/SchemaCard.vue
@@ -1,29 +1,34 @@
 <template>
   <div
-    class="p-6 relative border bg-white hover:bg-gray-some"
+    class="p-4 relative border bg-white hover:bg-gray-some"
     :class="{ 'border-gray-default': !selected, 'border-datagouv': selected }"
     :role="selectable ? 'option' : undefined"
     :aria-selected="selectable && selected"
   >
-    <h3 class="text-base font-bold mb-2 inline-flex items-center gap-2.5">
-      <RiDatabaseLine class="size-4" />
+    <h3 class="text-base font-bold mb-0 flex items-center gap-1">
+      <RiNodeTree class="size-4" />
       {{ schema.title }}
     </h3>
-    <p class="mb-3">
+    <p
+      v-for="label in schema.labels"
+      :key="label"
+      class="flex items-center gap-1 text-sm mb-0"
+    >
+      <span class="flex items-center gap-1 text-gray-medium">
+        <RiInformationLine class="size-3" /> {{ t('Label :') }}
+      </span>
+      <span>{{ label }}</span>
+    </p>
+    <p class="flex items-center gap-1 text-sm mb-0">
+      <span class="flex items-center gap-1 text-gray-medium">
+        <RiInformationLine class="size-3" /> {{ t('Standard :') }}
+      </span>
+      <span>{{ schema.schema_type }}</span>
+    </p>
+    <p class="text-sm mt-4 mb-0">
       {{ schema.description }}
     </p>
-    <div class="flex items-center gap-2">
-      <span class="fr-badge fr-badge--sm fr-badge--info">
-        {{ schema.schema_type }}
-      </span>
-      <span
-        v-for="label in schema.labels"
-        :key="label"
-        class="fr-badge fr-badge--sm"
-      >
-        {{ label }}
-      </span>
-    </div>
+    <div class="flex items-center gap-2" />
     <RiCheckLine
       v-if="selected"
       class="size-6 fill-datagouv absolute top-4 right-4"
@@ -32,7 +37,8 @@
 </template>
 
 <script setup lang="ts">
-import { RiCheckLine, RiDatabaseLine } from '@remixicon/vue'
+import { RiCheckLine, RiInformationLine, RiNodeTree } from '@remixicon/vue'
+import { useTranslation } from '../composables/useTranslation'
 import type { RegisteredSchema } from '../types/schemas'
 
 defineProps<{
@@ -40,4 +46,6 @@ defineProps<{
   selectable?: boolean
   selected?: boolean
 }>()
+
+const { t } = useTranslation()
 </script>
