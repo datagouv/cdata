8a91dc674302bca0aff8098c347e32a2d6699eb6
Thibaud Dauce <git@hadibut.fr>
2025-11-12 11:59:55 +0100

    Remove BannerSticky

--- components/BannerReadOnly.vue
+++ components/BannerReadOnly.vue
@@ -1,12 +1,16 @@
 <template>
-  <BannerSticky
-    v-if="config.public.readOnlyMode"
+  <SimpleBanner
+    v-if="config.public.readOnlyMode || true"
     type="warning"
   >
-    {{ $t('Pour des raisons de sécurité, la création de nouveaux contenus est actuellement désactivée.') }}
-  </BannerSticky>
+    <div class="fr-container">
+      {{ $t('Pour des raisons de sécurité, la création de nouveaux contenus est actuellement désactivée.') }}
+    </div>
+  </SimpleBanner>
 </template>
 
 <script setup lang="ts">
+import { SimpleBanner } from '@datagouv/components-next'
+
 const config = useRuntimeConfig()
 </script>
