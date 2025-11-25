d8c3560567f7ae41e0cda36c76d581c605b76233
maudetes <estelle.maudet@data.gouv.fr>
2025-11-25 08:53:34 +0100

    feat(harvest): add harvest item status filter

--- components/SearchableSelect.vue
+++ components/SearchableSelect.vue
@@ -81,7 +81,7 @@
         >
           <ComboboxOptions
             ref="popover"
-            class="z-10 mt-1 absolute max-h-60 min-w-80 w-full overflow-auto rounded-md bg-white text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm pl-0"
+            class="z-10 mt-1 absolute max-h-60 min-w-70 w-full overflow-auto rounded-md bg-white text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm pl-0"
           >
             <div
               v-if="!filteredAndGroupedOptions && query !== ''"
