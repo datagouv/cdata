a45f3813c036bba753325eb74538c6d5e4c8afe8
maudetes <estelle.maudet@data.gouv.fr>
2025-11-28 16:51:06 +0100

    Fix isOrgAdmin in harvester configuration
    Following https://github.com/datagouv/cdata/pull/790

--- pages/admin/harvesters/[id]/configuration.vue
+++ pages/admin/harvesters/[id]/configuration.vue
@@ -91,7 +91,7 @@
 
 <script setup lang="ts">
 import { RiDeleteBin6Line } from '@remixicon/vue'
-import { BannerAction, BrandedButton } from '@datagouv/components-next'
+import { BannerAction, BrandedButton, type Organization } from '@datagouv/components-next'
 import DescribeHarvester from '~/components/Harvesters/DescribeHarvester.vue'
 import JobPage from '~/components/Harvesters/JobPage.vue'
 import PreviewLoader from '~/components/Harvesters/PreviewLoader.vue'
@@ -107,7 +107,10 @@ const me = useMe()
 const sourceUrl = computed(() => `/api/1/harvest/source/${route.params.id}`)
 const { data: harvester, refresh } = await useAPI<HarvesterSource>(sourceUrl, { redirectOn404: true })
 
-const isOrgAdmin = computed(() => !harvester.value.organization || isUserOrgAdmin(me.value, harvester.value.organization))
+// we fetch full organization object if exists because we need members details for isOrgAdmin
+const organization = harvester.value.organization ? (await useAPI<Organization>(`/api/1/organizations/${harvester.value.organization.id}`)).data : null
+
+const isOrgAdmin = computed(() => !harvester.value.organization || isUserOrgAdmin(me.value, organization.value))
 
 const loading = ref(false)
 
