6bf50c5f9e046de6de06ae909d581c902c920739
Dorine Lambinet <dorine.lambinet@gmail.com>
2025-12-10 18:22:24 +0100

    Modification de la mise en page pour mettre en avant les diverses ressources à destination des administrations

--- pages/dataservices/index.vue
+++ pages/dataservices/index.vue
@@ -17,123 +17,214 @@
       :page-id="site.dataservices_page"
       main-color="brown-illustration"
     />
-    <div class="container flex flex-col sm:flex-row items-center  gap-2 py-16">
-      <div class="w-full">
-        <div class="max-w-md">
-          <div class="font-extrabold text-3xl">
-            {{ $t('Administrations, ') }}
-          </div>
-          <div class="text-3xl">
-            {{ $t('simplifiez les démarches administratives grâce aux API') }}
-          </div>
-          <div class="mt-2">
-            {{ $t('Des exemples d’outils et de l′accompagnement à votre disposition pour simplifier, avec de la donnée, les démarches des citoyens.') }}
-          </div>
-        </div>
+    <section class="container w-full pt-16">
+      <div>
+        <h2 class="font-extrabold text-3xl">
+          {{ $t('Des outils pour les administrations') }}
+        </h2>
+        <p class="text-2xl">
+          {{ $t('Acteurs publics, que vous soyez utilisateurs ou fournisseurs d’API,') }}<br/>
+          {{ $t('des ressources sont à votre disposition :') }}
+        </p>
       </div>
-      <div class="w-full grid grid-cols-2 gap-4">
-        <div class="bg-gray-lowest-2 flex flex-col items-center p-8 space-y-4">
-          <nuxt-img
-            src="/illustrations/book-2.svg"
-            class="h-20"
-          />
-
-          <div class="font-extrabold text-center">
-            {{ $t('La doctrine des API') }}
+       <div class="w-full grid md:grid-cols-3 gap-x-2.5 mb-10">
+        <div class="flex flex-col space-y-3 justify-center">
+           <div class="bg-gray-lowest-2 flex flex-col items-center px-5 py-12 space-y-4">
+            <div class=" flex items-center justify-center size-20 bg-white rounded-full">
+              <nuxt-img
+                src="/illustrations/book-2.svg"
+                class="w-18"
+              />
+            </div>
+            <div class="font-extrabold text-center text-lg">
+              {{ $t('La doctrine des API') }}
+            </div>
+            <div class="text-center">
+              {{ $t('Elle précise le cadre d’action et identifie les bonnes pratiques à poursuivre en matière d’usage et d’exposition d’API par les administrations.') }}
+            </div>
+            <BrandedButton
+              href="https://guides.data.gouv.fr/guide-data.gouv.fr/api/outils-pour-les-administrations/doctrine-des-api"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="secondary"
+              :icon="RiArrowRightLine"
+              :icon-right="true"
+              size="sm"
+              aria-label="Doctrine des API sur guides.data.gouv.fr"
+            >
+              Lire la doctrine
+            </BrandedButton>
           </div>
-          <div class="text-sm text-center">
-            {{ $t('Elle précise le cadre d’action et identifie les bonnes pratiques à poursuivre en matière d’usage et d’exposition d’API par les administrations') }}
+          
+          
+          <div class="bg-gray-lowest-2 flex flex-col items-center px-5 py-12 space-y-4">
+            <div class=" flex items-center justify-center size-20 bg-white rounded-full">
+              <nuxt-img
+                src="/illustrations/_keyboard.svg"
+                class="w-16"
+              />
+            </div>
+            <div class="font-extrabold text-center text-lg">
+            {{ $t('Publier vos API') }}
+            </div>
+            <div class="text-center">
+              {{ $t(`Data.gouv.fr vous permet d'exposer vos API en quelques clics.`) }}
+            </div>
+             <BrandedButton
+              href="https://entreprise.api.gouv.fr/"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="brown-illustration"
+              :icon="RiArrowRightLine"
+              :icon-right="true"
+              size="sm"
+              aria-label="Documentation API Entreprise"
+              >
+               Publier une API sur data.gouv.fr
+            </BrandedButton>
+            
           </div>
-           <BrandedButton
-            href="https://guides.data.gouv.fr/guide-data.gouv.fr/api/outils-pour-les-administrations/doctrine-des-api"
-            target="_blank"
-            rel="noopener noreferrer"
-            color="brown-illustration"
-            :icon="RiArrowRightLine"
-            :icon-right="true"
-            size="sm"
-            aria-label="Doctrine des API sur guides.data.gouv.fr"
-          >
-           Lire la doctrine
-          </BrandedButton>
         </div>
-        <div class="bg-gray-lowest-2 flex flex-col items-center p-8 space-y-4">
-          <nuxt-img
-            src="/illustrations/newspapper.svg"
-            class="h-20"
-          />
-
-          <div class="font-extrabold text-center">
+        <div class="flex flex-col space-y-3 justify-center">
+                   <div class="bg-gray-lowest-2 flex flex-col items-center px-5 py-12 space-y-4">
+            <div class=" flex items-center justify-center size-20 bg-white rounded-full">
+              <nuxt-img
+                src="/illustrations/schema.svg"
+                class="w-18"
+              />
+            </div>
+             <div class="font-extrabold text-center text-lg">
+            {{ $t('Un accompagnement') }}<br/>{{ $t('technique et métier') }}
+            </div>
+            <div class="text-center">
+              {{ $t(`Le pôle Data de la DINUM est disponible pour vous aider pour toutes vos questions relatives aux API. `) }}
+            </div>
+            <BrandedButton
+              href="https://guides.data.gouv.fr/guide-data.gouv.fr/api/outils-pour-les-administrations/accompagnement-humain"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="secondary"
+              :icon="RiArrowRightLine"
+              :icon-right="true"
+              size="sm"
+              aria-label="Accompagnement humain sur guides.data.gouv.fr"
+            >
+              Demander conseil
+            </BrandedButton>
+          </div>
+          <div class="bg-gray-lowest-2 flex flex-col items-center px-5 py-12 space-y-4">
+            <div class=" flex items-center justify-center size-20 bg-white rounded-full">
+              <nuxt-img
+                src="/illustrations/_keys.svg"
+                class="w-20"
+              />
+            </div>
+            <div class="font-extrabold text-center text-lg">
             {{ $t('Datapass') }}
+            </div>
+            <div class="text-center">
+              {{ $t(`En tant qu'utilisateur, Datapass accélère votre habilitation aux API sous accès restreint.`) }}<br/><br/>
+              {{ $t(`En tant que fournisseur d'API, Datapass est un outil clé en main d'instruction et de délivrance d’habilitations.`) }}
+            </div>
+            <BrandedButton
+              href="https://guides.data.gouv.fr/guide-data.gouv.fr/api/outils-pour-les-administrations/datapass-outil-dhabilitations"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="secondary"
+              :icon="RiArrowRightLine"
+              :icon-right="true"
+              size="sm"
+              aria-label="DataPass sur guides.data.gouv.fr"
+            >
+              En savoir plus
+            </BrandedButton>
           </div>
-          <div class="text-sm text-center">
-            {{ $t(`Datapass vous permet de demander et gérer vos habilitations à différentes données en accès restreint.`) }}
+
+        </div>
+        <div class="flex flex-col space-y-3 justify-center">
+<div class="bg-gray-lowest-2 flex flex-col items-center px-5 py-12 space-y-4">
+            <div class=" flex items-center justify-center size-20 bg-white rounded-full">
+              <nuxt-img
+                src="/illustrations/lightbulb.svg"
+                class="w-12"
+              />
+            </div>
+             <div class="font-extrabold text-center text-lg">
+            {{ $t('Simplifions.data.gouv.fr') }}
+            </div>
+            <div class="text-center">
+              {{ $t(`Un catalogue des API, données et solutions par cas d'usages pour vous aider à mettre en oeuvre le Dites-le nous une fois et la proactivité.`) }}
+            </div>
+            <BrandedButton
+              href="https://simplifions.data.gouv.fr/"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="brown-illustration"
+              :icon="RiExternalLinkFill"
+              :icon-right="true"
+              size="sm"
+              aria-label="Aller sur Simplifions.data — lien externe"
+            >
+              Consulter le catalogue
+            </BrandedButton>
           </div>
+          <div class="bg-gray-lowest-2 flex flex-col items-center px-5 py-12 space-y-4">
+            <div class=" flex items-center justify-center size-20 bg-white rounded-full">
+              <nuxt-img
+                src="/illustrations/_safe.svg"
+                class="w-20"
+              />
+            </div>
+            <div class="font-extrabold text-center text-lg">
+              {{ $t('API Entreprise et API Particulier') }}
+              </div>
+            <div class="text-center">
+              {{ $t(`Les données des particuliers, des entreprises et associations, accessibles depuis une même API pour les administrations, afin de simplifier les démarches administratives.`) }}
+            </div>    
            <BrandedButton
-            href="https://guides.data.gouv.fr/guide-data.gouv.fr/api/outils-pour-les-administrations/datapass-outil-dhabilitations"
-            target="_blank"
-            rel="noopener noreferrer"
-            color="secondary"
-            :icon="RiArrowRightLine"
-            :icon-right="true"
-            size="sm"
-            aria-label="DataPass sur guides.data.gouv.fr"
-          >
-           En savoir plus
-          </BrandedButton>
-        </div>
-        <div class="bg-gray-lowest-2 flex flex-col items-center p-8 space-y-4">
-          <nuxt-img
-            src="/illustrations/lightbulb.svg"
-            class="h-20"
-          />
-
-          <div class="font-extrabold text-center">
-            {{ $t('Le portail Simplifions.data.gouv') }}
+              href="https://entreprise.api.gouv.fr/"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="brown-illustration"
+              :icon="RiExternalLinkFill"
+              :icon-right="true"
+              size="sm"
+              aria-label="Documentation API Entreprise"
+              >
+               API Entreprise
+            </BrandedButton>
+            <BrandedButton
+              href="https://particulier.api.gouv.fr/"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="secondary"
+              :icon="RiExternalLinkFill"
+              :icon-right="true"
+              size="sm"
+              aria-label="Documentation API Particulier"
+              >
+               API Particulier
+            </BrandedButton>
+             <div class="text-center">              
+              {{ $t(`Fournisseurs d'API, les bouquets s'occupent de l'exposition et du support de vos API permettant la mise en oeuvre du Dites-le nous une fois.`) }}
+              
+            </div>
+            <BrandedButton
+              href="https://guides.data.gouv.fr/guide-data.gouv.fr/api/outils-pour-les-administrations/bouquets-api-entreprise-et-api-particulier#comment-rejoindre-les-bouquets-api-entreprise-api-particulier"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="secondary"
+              :icon="RiArrowRightLine"
+              :icon-right="true"
+              size="sm"
+              aria-label="Documentation API Particulier"
+              >
+               Proposer votre API
+            </BrandedButton>
           </div>
-          <div class="text-sm text-center">
-            {{ $t(`Des cas d'usages et des solutions pour simplifier les démarches des citoyens, entreprises et associations, grâce aux API et aux données.`) }}
-          </div>
-          <BrandedButton
-            href="https://simplifions.data.gouv.fr/"
-            target="_blank"
-            rel="noopener noreferrer"
-            color="brown-illustration"
-            :icon="RiExternalLinkFill"
-            :icon-right="true"
-            size="sm"
-            aria-label="Aller sur Simplifions.data — lien externe"
-          >
-           Consulter le site 
-          </BrandedButton>
-        </div>
-        <div class="bg-gray-lowest-2 flex flex-col items-center p-8 space-y-4">
-          <nuxt-img
-            src="/illustrations/show.svg"
-            class="h-20"
-          />
-
-          <div class="font-extrabold text-center">
-            {{ $t('Un accompagnement humain') }}
-          </div>
-          <div class="text-sm text-center">
-            {{ $t(`N’hésitez pas à contacter le pôle circulation de la donnée de la DINUM, qui est disponible pour vous aider pour toutes vos questions relatives aux APIs. `) }}
-          </div>
-                    <BrandedButton
-            href="https://guides.data.gouv.fr/guide-data.gouv.fr/api/outils-pour-les-administrations/accompagnement-humain"
-            target="_blank"
-            rel="noopener noreferrer"
-            color="secondary"
-            :icon="RiExternalLinkFill"
-            :icon-right="true"
-            size="sm"
-            aria-label="Accompagnement humain sur guides.data.gouv.fr"
-          >
-           En savoir plus
-          </BrandedButton>
         </div>
       </div>
-    </div>
+    </section>
     <EditoFooter
       color="purple"
       search-url="/dataservices/search"
