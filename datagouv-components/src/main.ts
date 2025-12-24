fa9522eb5e293667e70623a87bdc13e205883b29
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-12-24 16:41:18 +0100

    feat: handle transfer request notification

--- datagouv-components/src/main.ts
+++ datagouv-components/src/main.ts
@@ -3,8 +3,8 @@ import type { Activity, ActivityKey } from './types/activity.js'
 import type { PaginatedArray } from './types/api.js'
 import type { ContactPoint, ContactPointRole } from './types/contact_point.js'
 import type { Badge, Badges, TranslatedBadge } from './types/badges'
-import type { Dataset, DatasetV2, DatasetV2WithFullObject, NewDataset, Quality, Rel } from './types/datasets'
-import type { NewDataservice, Dataservice } from './types/dataservices'
+import type { DatasetReference, Dataset, DatasetV2, DatasetV2WithFullObject, NewDataset, Quality, Rel } from './types/datasets'
+import type { DataserviceReference, NewDataservice, Dataservice } from './types/dataservices'
 import type { AccessType, AccessAudience, AccessAudienceCondition, AccessAudienceType, WithAccessType, AccessTypeForm } from './types/access_types'
 import type { Frequency, Frequencies } from './types/frequency'
 import type { Granularity, Granularities, SpatialZone } from './types/granularity'
@@ -12,7 +12,7 @@ import type { Harvest } from './types/harvest'
 import type { License } from './types/licenses'
 import type { Member, MemberRole, NewOrganization, Organization, OrganizationOrSuggest, OrganizationReference, OrganizationSuggest } from './types/organizations'
 import type { Owned, OwnedWithId } from './types/owned'
-import type { NewReuse, Reuse, ReuseTopic, ReuseType } from './types/reuses'
+import type { ReuseReference, NewReuse, Reuse, ReuseTopic, ReuseType } from './types/reuses'
 import type { RegisteredSchema, Schema, SchemaDetails, SchemaField, SchemaPath, SchemaPublicationMode, SchemaResponseData, SchemaVersion, ValidataError } from './types/schemas'
 import type { TopicV2, TopicElement, TopicElementClass, TopicElementRel } from './types/topics'
 import type { CommunityResource, FileResourceFileType, RemoteResourceFileType, ResourceFileType, ResourceType, Resource } from './types/resources'
@@ -111,9 +111,11 @@ export type {
   CommunityResource,
   ContactPoint,
   ContactPointRole,
+  DatasetReference,
   Dataset,
   DatasetV2,
   DatasetV2WithFullObject,
+  DataserviceReference,
   Dataservice,
   NewDataservice,
   FileResourceFileType,
@@ -145,6 +147,7 @@ export type {
   Resource,
   ResourceFileType,
   ResourceType,
+  ReuseReference,
   Reuse,
   ReuseTopic,
   ReuseType,
