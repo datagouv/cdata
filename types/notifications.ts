fbddcd3b944e91889504dd672d2f772c8ecd26ec
Nicolas KEMPF <nicolas@conciergerie.dev>
2026-01-07 14:13:04 +0100

    feat: handle transfer request notification (#846)

--- types/notifications.ts
+++ types/notifications.ts
@@ -1,19 +1,28 @@
-import type { Organization, User } from '@datagouv/components-next'
+import type { DataserviceReference, DatasetReference, OrganizationReference, ReuseReference, UserReference } from '@datagouv/components-next'
 
 export type CommonNotification = {
   created_at: string
   handled_at: string | null
   id: string
   last_modified: string
-  user: User
+  user: UserReference
 }
 
 export type MembershipRequestNotification = CommonNotification & {
   details: {
     class: 'MembershipRequestNotificationDetails'
-    request_organization: Organization
-    request_user: User
+    request_organization: OrganizationReference
+    request_user: UserReference
   }
 }
 
-export type UserNotification = MembershipRequestNotification
+export type TransferRequestNotification = CommonNotification & {
+  details: {
+    class: 'TransferRequestNotificationDetails'
+    transfer_owner: OrganizationReference | UserReference
+    transfer_recipient: OrganizationReference | UserReference
+    transfer_subject: DatasetReference | DataserviceReference | ReuseReference
+  }
+}
+
+export type UserNotification = MembershipRequestNotification | TransferRequestNotification
