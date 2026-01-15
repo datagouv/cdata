82c72960387297644b545c0c7a109de4ed2d63a1
Thibaud Dauce <git@hadibut.fr>
2026-01-15 14:26:52 +0100

    feat: add elections page

---
+++ components/Elections/ElectionCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtLink
+    :to="to"
+    class="flex flex-col md:flex-row gap-2.5 p-4 rounded-sm border border-gray-silver hover:border-gray-medium transition-colors bg-white bg-none"
+  >
+    <img
+      :src="image"
+      :alt="label"
+      class="w-full md:w-[225px] md:h-[130px] object-cover object-left-bottom flex-shrink-0 border border-gray-default rounded-lg"
+    >
+    <div class="flex-1">
+      <h3 class="font-bold text-base leading-normal mb-2">
+        {{ title }}
+      </h3>
+      <p class="font-normal text-sm leading-normal">
+        {{ description }}
+      </p>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label: string
+  title: string
+  description: string
+  to: string
+  image: string
+}>()
+</script>
