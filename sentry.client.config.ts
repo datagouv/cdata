5bba71fce34b412f7a34586b7716ef296c731a80
Thibaud Dauce <git@hadibut.fr>
2026-01-22 17:11:30 +0100

    build with commit id instead of having it at runtime

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nuxt'
 
 Sentry.init({
   dsn: useRuntimeConfig().public.sentry.dsn,
-  release: useRuntimeConfig().public.commitId,
+  release: useAppConfig().commitId,
   environment: useRuntimeConfig().public.apiBase.replace(/https?:\/\//, ''),
   tracesSampleRate: 0.1, // Adjust this value in production or use tracesSampler for finer control
   profilesSampleRate: 0.1, // Adjust this value in production
