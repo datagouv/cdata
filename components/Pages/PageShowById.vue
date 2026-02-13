43e4420b8a754340428845ce0619bfc3b94af672
Thibaud Ollagnier <ThibaudDauce@users.noreply.github.com>
2026-02-13 09:06:02 +0100

    fix: double fetch in post page (#937)
    From https://www.data.gouv.fr/elections click on a page. We do two API
    request:
    1. fetch the `Post` (with blocs inside `page_as_content`
    2. fetch the `Page` (we already have the blocs…)

--- components/Pages/PageShowById.vue
+++ components/Pages/PageShowById.vue
@@ -1,39 +1,16 @@
 <template>
-  <div
+  <PageShow
     v-if="page"
-    class="relative"
-  >
-    <div
-      v-if="isMeAdmin() && !isEditing"
-      class="absolute top-4 right-4 z-50"
-    >
-      <BrandedButton
-        color="warning"
-        :icon="RiEdit2Line"
-        size="xs"
-        @click="enterEditMode"
-      >
-        {{ $t('Modifier') }}
-      </BrandedButton>
-    </div>
-    <PageShow
-      :page
-      :edit="isEditing"
-      :main-color
-      @save="savePage"
-      @cancel="exitEditMode"
-    />
-  </div>
+    :page
+    editable
+    :main-color
+  />
 </template>
 
 <script setup lang="ts">
 import type { ComponentProps } from 'vue-component-type-helpers'
-import { BrandedButton } from '@datagouv/components-next'
-import { RiEdit2Line } from '@remixicon/vue'
-import { toast } from 'vue-sonner'
 import PageShow from './PageShow.vue'
 import type { Page } from '~/types/pages'
-import { isMeAdmin } from '~/utils/auth'
 
 const props = withDefaults(defineProps<{
   pageId: string
@@ -42,35 +19,5 @@ const props = withDefaults(defineProps<{
   mainColor: 'primary',
 })
 
-const { $api } = useNuxtApp()
-const { t } = useTranslation()
-const route = useRoute()
-const router = useRouter()
-
-const { data: page, refresh } = await useAPI<Page>(`/api/1/pages/${props.pageId}`)
-
-const isEditing = computed(() => route.query.edit === 'true')
-
-async function savePage(updatedPage: Page) {
-  try {
-    await $api(`/api/1/pages/${props.pageId}/`, {
-      method: 'PUT',
-      body: updatedPage,
-    })
-    toast.success(t('Page sauvegardée'))
-    await refresh()
-    exitEditMode()
-  }
-  catch {
-    toast.error(t('Erreur lors de la sauvegarde'))
-  }
-}
-
-function enterEditMode() {
-  router.push({ query: { ...route.query, edit: 'true' } })
-}
-
-function exitEditMode() {
-  router.replace({ query: { ...route.query, edit: undefined } })
-}
+const { data: page } = await useAPI<Page>(`/api/1/pages/${props.pageId}`)
 </script>
