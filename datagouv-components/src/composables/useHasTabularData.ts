62674c158415ac7faa4ad6e4dba53b625158fc9e
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2026-02-24 13:20:54 +0100

    fix: type

--- datagouv-components/src/composables/useHasTabularData.ts
+++ datagouv-components/src/composables/useHasTabularData.ts
@@ -1,16 +1,11 @@
 import { useComponentsConfig } from '../config'
+import type { Resource } from '../types/resources'
 
-/**
- * Composable to determine if a resource has tabular data.
- * This is used to show the "Données" tab for tabular files AND the "Structure des données" tab (for tabular data structure).
- *
- * @returns An object containing the hasTabularData function.
- */
 export const useHasTabularData = () => {
   const config = useComponentsConfig()
 
   return {
-    hasTabularData: (resource: { extras: Record<string, unknown>, filetype?: string }) => {
+    hasTabularData: (resource: Resource) => {
       return (
         config.tabularApiUrl
         && resource.extras['analysis:parsing:parsing_table']
