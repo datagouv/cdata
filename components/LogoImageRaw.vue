eec4c0403a13359297016845ee226f1cbfa3c149
Thibaud Dauce <git@hadibut.fr>
2026-02-26 14:14:54 +0100

    remove old files

--- components/LogoImageRaw.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <!-- We translate to center the SVG which is asymetric. -->
-  <img
-    src="/nuxt_images/logo_text.svg"
-    class="inline -translate-y-[15%]"
-    alt=""
-  >
-</template>
