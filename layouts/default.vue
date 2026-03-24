b45275a4485713608eb9b4308aca9fc2d57ca488
Thibaud Dauce <git@hadibut.fr>
2026-03-24 08:40:39 +0100

    revert layout hack for og image

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-// Workaround: layout setup re-runs client-side when navigating between layouts (e.g. fluid → default),
-// but defineOgImage throws in client context. This is likely a nuxt-og-image bug.
-// See https://github.com/nuxt-modules/og-image/pull/293
-if (import.meta.server) {
-  defineOgImage('Default.takumi')
-}
+defineOgImage('Default.takumi')
 </script>
 
 <template>
