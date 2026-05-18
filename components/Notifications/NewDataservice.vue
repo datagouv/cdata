1699b9e816a474d3db406613d2fbca126a480e11
Nicolas KEMPF <nicolas@conciergerie.dev>
2026-05-18 09:49:09 +0200

    feat(notifications): add new notifications (#1076)

---
+++ components/Notifications/NewDataservice.vue
@@ -0,0 +1,26 @@
+<template>
+  <NotificationLayout
+    :icon="RiTerminalLine"
+    :title="$t('Nouvelle API')"
+    :notification="notification"
+    :title-link="notification.details.dataservice.self_web_url"
+    :title-link-title="$t('Voir l\'API')"
+  >
+    <p class="m-0 text-xs truncate">
+      {{ notification.details.dataservice.title }}
+    </p>
+    <p class="m-0 text-xs text-gray-medium truncate">
+      {{ $t('sur') }} {{ notification.details.dataset.title }}
+    </p>
+  </NotificationLayout>
+</template>
+
+<script setup lang="ts">
+import { RiTerminalLine } from '@remixicon/vue'
+import type { DataserviceCreatedNotification } from '~/types/notifications'
+import NotificationLayout from './NotificationLayout.vue'
+
+defineProps<{
+  notification: DataserviceCreatedNotification
+}>()
+</script>
