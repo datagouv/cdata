a7d4a1e65429ccdd58fb88874e9fb6e80cfbd3bc
Samuel-Zacharie FAURE <samuel.faure.dev@gmail.com>
2026-05-20 08:59:34 +0200

    feat: enhance contact display (#1075)
    fix https://github.com/datagouv/data.gouv.fr/issues/1907
    
    Screens:
    
    <img width="1628" height="1032" alt="screen1"
    src="https://github.com/user-attachments/assets/5cb2e9c4-fcfc-47a5-b9c3-7834d494b36a"
    />
    <img width="1451" height="1041" alt="screen2"
    src="https://github.com/user-attachments/assets/44182414-73c2-4c17-8b8c-201653ab4dd1"
    />

--- components/AccessTypes/AccessTypePanel.vue
+++ components/AccessTypes/AccessTypePanel.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="space-y-1">
-    <dt class="text-gray-plain font-bold">
+    <dt
+      class="text-gray-plain font-bold"
+      :class="{ 'sr-only': hideAccessLabel }"
+    >
       {{ $t('Accès') }}
     </dt>
     <dd class="p-0">
@@ -11,19 +14,6 @@
       <template v-else>
         {{ $t('Non spécifié') }}
       </template>
-      <div
-        v-if="object.authorization_request_url"
-        class="mt-2.5"
-      >
-        <a
-          :href="object.authorization_request_url"
-          rel="ugc nofollow noopener"
-          target="_blank"
-          class="fr-text--sm fr-link"
-        >
-          {{ $t("Faire une demande d'habilitation") }}
-        </a>
-      </div>
     </dd>
     <template v-if="object.access_type === 'restricted'">
       <dt class="text-gray-plain font-bold mt-2.5">
@@ -51,16 +41,32 @@
         </template>
       </dd>
     </template>
+    <div
+      v-if="object.authorization_request_url"
+      class="mt-2.5"
+    >
+      <BrandedButton
+        color="primary"
+        size="sm"
+        :href="object.authorization_request_url"
+        new-tab
+      >
+        {{ $t("Demander un accès") }}
+      </BrandedButton>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { AccessAudience, AccessAudienceType, WithAccessType } from '@datagouv/components-next'
+import { BrandedButton, type AccessAudience, type AccessAudienceType, type WithAccessType } from '@datagouv/components-next'
 import AccessTypeBadge from './AccessTypeBadge.vue'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   object: WithAccessType
-}>()
+  hideAccessLabel?: boolean
+}>(), {
+  hideAccessLabel: false,
+})
 
 const accessAudiences = computed(() => (['local_authority_and_administration', 'company_and_association', 'private'] as Array<AccessAudienceType>)
   .map(type => props.object.access_audiences.find(a => a.role === type))
