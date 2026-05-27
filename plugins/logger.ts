e920f3ebc8b3dd5c75b6a55f3d4403d810ff879b
maudetes <estelle.maudet@data.gouv.fr>
2026-05-27 15:09:52 +0200

    fix lint

--- plugins/logger.ts
+++ plugins/logger.ts
@@ -6,21 +6,20 @@ let __logger_plugin_initialized = false
 export default defineNuxtPlugin(() => {
   if (__logger_plugin_initialized) return
   if (process.env.NODE_ENV !== 'production') return
-    __logger_plugin_initialized = true
+  __logger_plugin_initialized = true
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const createLogger = (originalMethod: (...args: any[]) => void) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const createLogger = (originalMethod: (...args: any[]) => void) => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      return function (...args: any[]) {
-        const timestamp = new Date().toISOString()
-        originalMethod(`[${timestamp}]`, ...args)
-      }
+    return function (...args: any[]) {
+      const timestamp = new Date().toISOString()
+      originalMethod(`[${timestamp}]`, ...args)
     }
-
-    console.log = createLogger(console.log)
-    console.error = createLogger(console.error)
-    console.warn = createLogger(console.warn)
-    console.info = createLogger(console.info)
-    console.debug = createLogger(console.debug)
   }
+
+  console.log = createLogger(console.log)
+  console.error = createLogger(console.error)
+  console.warn = createLogger(console.warn)
+  console.info = createLogger(console.info)
+  console.debug = createLogger(console.debug)
 })
