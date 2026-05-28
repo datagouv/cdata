adb6e1fe155150637382834c924870ac34a3c2c8
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2026-05-28 18:02:28 +0200

    refactor: clarify tabular API

--- datagouv-components/src/components/ResourceExplorer/ResourceExplorerViewer.vue
+++ datagouv-components/src/components/ResourceExplorer/ResourceExplorerViewer.vue
@@ -297,12 +297,13 @@
                 </template>
               </dl>
             </div>
-            <div v-if="tab.key === 'swagger'">
+            <div v-if="tab.key === 'api'">
               <div class="fr-mb-4w">
                 <p>{{ t("Cette API est générée automatiquement par {platform} à partir du fichier.", { platform: config.name }) }}</p>
                 <p>{{ t("- Si le fichier est modifié, l'API sera mise à jour et sa structure pourra changer.") }}</p>
                 <p>{{ t("- Si le fichier est supprimé, l'API sera également supprimée.") }}</p>
                 <p>{{ t("Pour des usages pérennes, prévoyez que cette API dépend directement du fichier source.") }}</p>
+                <p>{{ t("L'URL de base de l'API est https://tabular-api.data.gouv.fr") }}</p>
               </div>
               <OpenApiViewer
                 v-if="hasTabularData"
