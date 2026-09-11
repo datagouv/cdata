2cc1430d887ce9e8497bfe87c82573f629eaa15c
Thibaud Ollagnier <ThibaudDauce@users.noreply.github.com>
2026-09-11 11:51:20 +0200

    feat: show the last comment and title in discussion admin table (#1225)
    Fix https://github.com/datagouv/data.gouv.fr/issues/2070

--- components/Discussions/ThreadModal.vue
+++ components/Discussions/ThreadModal.vue
@@ -1,27 +1,15 @@
 <template>
   <ModalWithButton
-    v-if="!thread.closed"
-    :title="$t('Répondre')"
+    v-model="opened"
+    :title="thread.title"
     size="lg"
   >
-    <template #button="{ attrs, listeners }">
-      <BrandedButton
-        :icon="RiChatNewLine"
-        size="xs"
-        color="tertiary"
-        icon-only
-        keep-margins-even-without-borders
-        :title="$t('Répondre à la discussion')"
-        :aria-label="$t('Répondre à la discussion {title}', { title: thread.title })"
-        v-bind="attrs"
-        v-on="listeners"
-      />
-    </template>
     <template #default="{ close }">
       <DiscussionCard
         v-if="subject"
         :thread
         :subject
+        respond-immediately
         @change="respond(close)"
       />
     </template>
@@ -29,8 +17,6 @@
 </template>
 
 <script setup lang="ts">
-import { RiChatNewLine } from '@remixicon/vue'
-import { BrandedButton } from '@datagouv/components-next'
 import DiscussionCard from '~/components/Discussions/DiscussionCard.vue'
 import type { DiscussionSubjectTypes, Thread } from '~/types/discussions'
 
@@ -42,6 +28,8 @@ defineProps<{
 const emit = defineEmits<{
   (e: 'responded'): void
 }>()
+
+const opened = defineModel<boolean>()
 
 function respond(close: () => void) {
   emit('responded')
